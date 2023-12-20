dda45a999cfa213a49a914436dc78137f0d24a21
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-21 00:22:07 +0530

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Employee Wage Computaion Program");
+
+        int IS_PRESENT=1;
+        int EMP_PER_HOUR=20;
+        int FULL_DAY_HOUR=8;
+
+        int attendance = (int)(Math.floor(Math.random() * 2.0));
+        int wages=0;
+
+        if(attendance==1)
+        {
+            wages=EMP_PER_HOUR*FULL_DAY_HOUR;
+            System.out.println("Employee is present and his wage is " + wages);
+        }
+
+        else 
+        {
+            System.out.println("Employee is absent today");
+        }
+
     }
 }
