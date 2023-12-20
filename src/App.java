2d964b9fb29b9b0fdf26f2b9963e9b7d9f90b1a3
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-21 00:45:04 +0530

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Employee Wage Computaion Program");
+        System.out.println("Welcome to Employee Wage Computation.");
+
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        int FULL_TIME_WORKING_HOURS = 8;
+        int PART_TIME_WORKING_HOURS = 4;
+        int TOTAL_WAGE=0;
+
+        for (int i = 0; i < 30; i++) {
+
+            int attendance = (int) (Math.floor(Math.random() * 3.0));
+
+            int empHours = 0;
+            int empWage = 0;
+
+            if (attendance == IS_FULL_TIME) {
+                System.out.println("Employee is working full time.");
+                empHours = FULL_TIME_WORKING_HOURS;
+            } else {
+                if (attendance == IS_PART_TIME) {
+                    System.out.println("Employee is working part time.");
+                    empHours = PART_TIME_WORKING_HOURS;
+                } else {
+                    System.out.println("Employee is on leave.");
+                    empHours = 0;
+                }
+            }
+
+            empWage = EMP_RATE_PER_HOUR * empHours;
+            TOTAL_WAGE+=empWage;
+        }
+
+        System.out.println("Employee Wage for the day is: " + TOTAL_WAGE);
+
     }
 }
