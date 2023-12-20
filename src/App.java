485d681617bcb3fcd7daa5a3b683442cc58a1374
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-21 00:39:07 +0530

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
 public class App {
+
+    public static final int IS_PART_TIME = 1;
+
+    public static final int IS_FULL_TIME = 2;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Employee Wage Computaion Program");
+        System.out.println("Welcome to Employee Wage Computation.");
+
+        int attendance = (int) (Math.floor(Math.random() * 3.0));
+        int FULL_TIME_WORKING_HOURS = 8;
+        int PART_TIME_WORKING_HOURS = 4;
+        int EMP_RATE_PER_HOUR = 20;
+
+        int empHours = 0;
+        int empWage = 0;
+
+        switch (attendance) {
+            case IS_FULL_TIME:
+                System.out.println("Employee is working full time.");
+                empHours = FULL_TIME_WORKING_HOURS;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee is working part time.");
+                empHours = PART_TIME_WORKING_HOURS;
+                break;
+            default:
+                System.out.println("Employee is on leave.");
+                empHours = 0;
+        }
+
+        empWage = EMP_RATE_PER_HOUR * empHours;
+
+        System.out.println("Employee Wage for the day is: " + empWage);
+
     }
 }
